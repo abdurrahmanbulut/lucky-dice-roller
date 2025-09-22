f9efd6b16069c18edfea332943abb635d79263ea
Abdurrahman BULUT <abdurrahmanbulut08@gmail.com>
2025-09-22 14:46:22 +0300

    jks updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,45 +8,20 @@ plugins {
 
 android {
     namespace = "com.bulut.luckyDiceRoller"
-    compileSdk = 34
+    compileSdk = 36
+    defaultConfig.minSdk = 24
+    defaultConfig.targetSdk = 36
+    defaultConfig.versionCode = 3
+    defaultConfig.versionName = "1.2"
+    defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    kotlin.jvmToolchain(17)
+    buildFeatures.compose = true
+    packaging.resources.excludes += "META-INF/{AL2.0,LGPL2.1}"
 
     defaultConfig {
         applicationId = "com.bulut.luckyDiceRoller"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
